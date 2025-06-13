08f89b9564d3e7a7e0778e37428fdbfd33eb0824
Sean Weber <sweber@pltw.org>
2025-06-13 13:40:01 -0400

    more debugging

--- slippc/src/parser.cpp
+++ slippc/src/parser.cpp
@@ -806,9 +806,9 @@ namespace slip {
     DOUT1("  Saved to " << outfilename);
     std::cout << "DEBUG 2" << std::endl;
 
-  //  DOUT1("  Saving Player Frames");
-  //  std::ofstream ofile3;
-  //  ofile3.open(framesfilename);
+    DOUT1("  Saving Player Frames");
+    std::ofstream ofile3;
+    ofile3.open(framesfilename);
   //  ofile3 << playerFramesAsJson() << std::endl;
   //  ofile3.close();
   //  DOUT1("  Saved to " << framesfilename);
