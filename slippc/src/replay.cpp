897cdd908c5e41adb763285b0b6c8ded7de1470e
Sean Weber <sweber@pltw.org>
2025-06-12 21:08:09 -0400

    fix indentations

--- slippc/src/replay.cpp
+++ slippc/src/replay.cpp
@@ -308,8 +308,8 @@ std::string SlippiReplay::replayAsJson(bool delta) {
         ss << SPACE[ILEV*2] << "{";
         int a = 0; //True for only the first thing output per line
         ss << JSTR(2,"match_id" ,s.start_time)       << ",\n";
-        ss << JUIN(1,"spawn_id" ,s.item[i].spawn_id) << ",\n";
-        ss << JUIN(1,"item_type",s.item[i].type)     << ",\n";
+        ss << JUIN(2,"spawn_id" ,s.item[i].spawn_id) << ",\n";
+        ss << JUIN(2,"item_type",s.item[i].type)     << ",\n";
         ss << JEND(a) << JUIN(2,"frame"      ,s.item[i].frame[f].frame);
         if (ICHANGED(state))
           ss << JEND(a) << JUIN(2,"state"      ,s.item[i].frame[f].state);
