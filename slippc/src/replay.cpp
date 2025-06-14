c3a51f90ea7991c8ac45b3d4cab2f220764dc567
Sean Weber <sweber@pltw.org>
2025-06-13 22:20:17 -0400

    implement separate settings file

--- slippc/src/replay.cpp
+++ slippc/src/replay.cpp
@@ -251,12 +251,12 @@ std::string SlippiReplay::settingsAsJson() {
   ss << JEND(a) << JUIN(2,"stage"          ,s.stage);
   ss << JEND(a) << JUIN(2,"end_type"       ,s.end_type);
   for (unsigned i = 0; i < 3; i++) {
-    std::cout << "DEBUG " << JSTR(2,"tag_code"    ,escape_json(s.player[i].tag_code));
+    std::cout << "DEBUG " << JSTR(2,"tag_code"    ,escape_json(s.player[i].tag_code)) << "\n";
     std::cout << "DEBUG " << std::to_string(i) << ": " << std::to_string(s.player[i].ext_char_id) << "\n";
 
     if (s.player[i].player_type == 3) {
-       std::cout << JSTR(2, ("player_" + std::to_string(i) + "_code") ,s.player[i].tag_code);
-       ss << JEND(a) << JSTR(2, ("player_" + std::to_string(i) + "_code") ,s.player[i].tag_code);
+       std::cout     << JSTR(2, ("player_" + std::to_string(i) + "_code")     ,s.player[i].tag_code);
+       ss << JEND(a) << JSTR(2, ("player_" + std::to_string(i) + "_code")     ,s.player[i].tag_code);
        ss << JEND(a) << JINT(2, ("player_" + std::to_string(i) + "_ext_char") ,s.player[i].ext_char_id);
     }
   }
