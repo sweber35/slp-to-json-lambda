b06d9bd8b8960bfcffaacd3e6fc45be1aa8789bf
Sean Weber <sweber@pltw.org>
2025-06-17 18:04:38 -0400

    debugging the old fashion way

--- slippc/src/replay.cpp
+++ slippc/src/replay.cpp
@@ -299,35 +299,35 @@ arrow::Status SlippiReplay::playerFramesAsParquet() {
     self_air_x_a, self_air_y_a, attack_x_a, attack_y_a, self_grd_x_a
   });
 
-  try {
-    logError("Opening Parquet output stream...");
-    std::shared_ptr<arrow::io::FileOutputStream> outfile;
-    PARQUET_ASSIGN_OR_THROW(outfile, arrow::io::FileOutputStream::Open("/tmp/frames.parquet"));
-
-    std::shared_ptr<arrow::io::OutputStream> outstream =
-      std::static_pointer_cast<arrow::io::OutputStream>(outfile);
-
-    logError("Setting writer properties...");
-
-    std::shared_ptr<parquet::WriterProperties> writer_properties =
-      parquet::WriterProperties::Builder()
-        .compression(parquet::Compression::SNAPPY)
-        ->build();
-
-    logError("Calling WriteTable...");
-    PARQUET_THROW_NOT_OK(
-      parquet::arrow::WriteTable(*table, arrow::default_memory_pool(), outstream, 1024, writer_properties)
-    );
-  } catch (const parquet::ParquetException& e) {
-    std::cerr << "[ParquetException] " << e.what() << std::endl;
-    return arrow::Status::ExecutionError("ParquetException: ", e.what());
-  } catch (const std::exception& e) {
-    std::cerr << "[std::exception] " << e.what() << std::endl;
-    return arrow::Status::ExecutionError("std::exception: ", e.what());
-  } catch (...) {
-    std::cerr << "[Unknown error] during Parquet file write." << std::endl;
-    return arrow::Status::ExecutionError("Unknown error during Parquet write");
-  }
+//   try {
+//     logError("Opening Parquet output stream...");
+//     std::shared_ptr<arrow::io::FileOutputStream> outfile;
+//     PARQUET_ASSIGN_OR_THROW(outfile, arrow::io::FileOutputStream::Open("/tmp/frames.parquet"));
+//
+//     std::shared_ptr<arrow::io::OutputStream> outstream =
+//       std::static_pointer_cast<arrow::io::OutputStream>(outfile);
+//
+//     logError("Setting writer properties...");
+//
+//     std::shared_ptr<parquet::WriterProperties> writer_properties =
+//       parquet::WriterProperties::Builder()
+//         .compression(parquet::Compression::SNAPPY)
+//         ->build();
+//
+//     logError("Calling WriteTable...");
+//     PARQUET_THROW_NOT_OK(
+//       parquet::arrow::WriteTable(*table, arrow::default_memory_pool(), outstream, 1024, writer_properties)
+//     );
+//   } catch (const parquet::ParquetException& e) {
+//     std::cerr << "[ParquetException] " << e.what() << std::endl;
+//     return arrow::Status::ExecutionError("ParquetException: ", e.what());
+//   } catch (const std::exception& e) {
+//     std::cerr << "[std::exception] " << e.what() << std::endl;
+//     return arrow::Status::ExecutionError("std::exception: ", e.what());
+//   } catch (...) {
+//     std::cerr << "[Unknown error] during Parquet file write." << std::endl;
+//     return arrow::Status::ExecutionError("Unknown error during Parquet write");
+//   }
 
   return arrow::Status::OK();
 }
